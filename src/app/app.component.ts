350d24462b90786b643a097129f12805f6db29ad
ndungub <ndungub@gmail.com>
2017-08-07 18:06:50 +0300

    home page look and feel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,46 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import {  MenuController, Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  navigatePages
+  otherPages
+  accountPages
+  rootPage:any = LoginPage;
+  private menu: MenuController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, menu: MenuController, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.menu = menu;
+      this.navigatePages = [
+        			{ title: 'Home', component: LoginPage, icon: 'home' },
+        			{ title: 'Evaluation', component: LoginPage, icon: 'pie' },
+        			{ title: 'Loans', component: LoginPage, icon: 'medal' },
+        			{ title: 'Payments', component: LoginPage, icon: 'cash' }
+       ];
+      this.otherPages = [
+                			{ title: 'Support', component: LoginPage, icon: 'settings' },
+                			{ title: 'About Us', component: LoginPage, icon: 'information-circle' }
+               ];
+      this.accountPages = [
+             			{ title: 'Sign Out', component: LoginPage, icon: 'log-out' }
+            ];
+
     });
   }
+  
+  openPage(page) {
+		this.menu.close();
+  };
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,66 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,App, LoadingController, ToastController } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { LoginPage } from '../login/login';
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  loading: any;
+  isLoggedIn: boolean = false;
+  
+  constructor(public app: App, public navCtrl: NavController, public authService: AuthServiceProvider, public loadingCtrl: LoadingController, private toastCtrl: ToastController) {
+    if(localStorage.getItem("token")) {
+      this.isLoggedIn = true;
+    }
   }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  
+  logout() {
+	    this.authService.logout().then((result) => {
+	      this.loading.dismiss();
+	      let nav = this.app.getRootNav();
+	      nav.setRoot(LoginPage);
+	    }, (err) => {
+	      this.loading.dismiss();
+	      this.presentToast(err);
+	    });
+	  }
+
+	  showLoader(){
+	    this.loading = this.loadingCtrl.create({
+	        content: 'Authenticating...'
+	    });
+
+	    this.loading.present();
+	  }
+
+	  presentToast(msg) {
+	    let toast = this.toastCtrl.create({
+	      message: msg,
+	      duration: 3000,
+	      position: 'bottom',
+	      dismissOnPageChange: true
+	    });
+
+	    toast.onDidDismiss(() => {
+	      console.log('Dismissed toast');
+	    });
+
+	    toast.present();
+	  }
+
 
 }
